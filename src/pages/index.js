bd2eb9dc534280f1e5930c3e95eec999ffc9e11e
learninglab-studio <studio@learninglab.xyz>
2023-02-20 20:32:29 -0500

    Updates:

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import Indie from '@/components/fonts/indie'
 import SlackImage from '@/components/tests/SlackImage'
 
 const inter = Inter({ subsets: ['latin'] })
