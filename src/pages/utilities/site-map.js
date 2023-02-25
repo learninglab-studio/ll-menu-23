55d3dee6577faf4087a6da31dfef873c60c2ddde
learninglab-studio <studio@learninglab.xyz>
2023-02-25 15:40:01 -0500

    Updates:

--- src/pages/utilities/site-map.js
+++ src/pages/utilities/site-map.js
@@ -1,6 +1,6 @@
 import fs from "fs"
 import path from "path"
-import Stringify from "@/components/utilities/Stringify"
+import BStringify from "@/components/utilities/BStringify"
 import Link from "next/link"
 
 export default function SiteMap (props) {
