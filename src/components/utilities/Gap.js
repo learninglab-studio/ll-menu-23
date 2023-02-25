f575ec1a49ba8eea5a9a909c69a8d1767bef958b
learninglab-studio <studio@learninglab.xyz>
2023-02-25 15:32:46 -0500

    Updates: spacer direct

--- src/components/utilities/Gap.js
+++ src/components/utilities/Gap.js
@@ -2,7 +2,7 @@ const Gap = (props) => {
     let w = props.width ? `${props.width}px` : "0px"
     let h = props.height ? `${props.height}px` : "0px"
     return (
-        <div style={{width: w height: h}}></div>
+        <div style={{width: w, height: h}}></div>
     )
 }
 
