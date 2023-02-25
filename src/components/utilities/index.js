30a74d4b3feb98bbf6fa544e4a7727abcd045149
learninglab-studio <studio@learninglab.xyz>
2023-02-25 15:15:46 -0500

    Updates:

--- src/components/utilities/index.js
+++ src/components/utilities/index.js
@@ -1,5 +1,5 @@
 // components/index.js
 export { default as CodeBlock } from './CodeBlock.js';
 // export { default as SiteMap } from './SiteMap';
-export { default as Spacer } from './Spacer.js';
+export { default as Spacer } from './spacer.js';
 export { default as Stringify } from './Stringify.js';
